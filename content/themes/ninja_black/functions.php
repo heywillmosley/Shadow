2d0bb759be8eec9e1558f69cec795ed9f02cd03f
William Mosley <will@livesuperamazing.com>
2013-06-18 15:57:40 -0400

    Revert "Added Shadow v1.1.1"
    This reverts commit 107d5b94ca1ae66d86ba1a5341822b150d757fc4.

--- content/themes/ninja_black/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// REQUIRED FOR NINJA BLACK TO WORK
-require_once (get_stylesheet_directory() . "/classes/class_lib.php");
-require_once (get_stylesheet_directory() . "/functions/function_lib.php");
