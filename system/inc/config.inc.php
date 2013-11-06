c1cf91862defa50a90b219656f1c574ff9f5b29f
William Mosley <wmosley@kingbio.com>
2013-11-06 12:13:31 -0500

    fixed #52

--- system/inc/config.inc.php
+++ system/inc/config.inc.php
@@ -40,7 +40,7 @@
  * -Added Basic Product Catalog
  * -Implement Pilot Interface
  */
-	define('SYS_VER', '1.3.0.1');
+	define('SYS_VER', '1.3.0.2');
 
 	$release =  str_replace( '-alpha', '', str_replace( '-beta', '', str_replace( '.', '', substr(SYS_VER, 0, strrpos(SYS_VER, '.') ) ) ) );
 	$sprint = substr(SYS_VER, strrpos(SYS_VER, '.') + 0);
@@ -1537,6 +1537,10 @@ if( SHDW )
 	catch(PDOException $e) {  
 		exceptionHandler( $e ); 
 	} 
+	
+	# Set default timezone to UTC
+	date_default_timezone_set('UTC');
+	
 } // end SHDW
 
 # One Second
